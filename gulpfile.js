45057618093a85199f86efcf1e9066d15ea7453f
Pablo <pablo.aguilar.wp@gmail.com>
2017-06-29 15:30:22 -0400

    added Coffee script files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp =  require('gulp'),
-    gutil = require('gulp-util');
-
-gulp.task('log',function(){
-  gutil.log('workflows are Sweeet');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
+var coffeeSource = 'components/coffee/*.coffee'
+gulp.task('coffee',function(){
+  gulp.src(coffeeSource)
+    .pipe(coffee({bare:true})
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });
