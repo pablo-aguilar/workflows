2301e23e00f623a8b95409fbcabb0ed0d7005e2f
Pablo <pablo.aguilar.wp@gmail.com>
2017-06-30 15:13:02 -0400

    added uglify

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var gulp =  require('gulp'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
     concat = require('gulp-concat'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
     connect = require('gulp-connect');
 
 var env,
@@ -72,6 +74,7 @@ gulp.task('js', function (){
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
+    .pipe(gulpif(env=== 'production',uglify()))
     .pipe(gulp.dest(outputDir + 'js'))
     .pipe(connect.reload())
 });
